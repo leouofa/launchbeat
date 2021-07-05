b88298f6d1b4df2b54f5bfda10aa5bef3ff61abf
Leonid Medovyy <leonid@storypro.io>
2021-07-05 09:04:49 -0700

    setting up the homepage text

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -4,15 +4,12 @@ class PageController < ApplicationController
   before_action :setup_grid
 
   def welcome
-    @bank = Theory::Bank.new
-    @c_maj_scale = Theory::Interval.new(root: 1, scale_pattern: [1, 3, 5, 6, 8, 10, 12])
   end
 
   def notes
   end
 
   def majors
-
   end
 
   private
@@ -21,9 +18,5 @@ class PageController < ApplicationController
     @bank = Theory::Bank.new
     session[:grid_width] = 8 if session[:grid_width].nil?
     @grid = Controller::Grid.new(width: session[:grid_width], bank: @bank).build.reverse
-
-    # we are reversing the grid to make displaying easier (since we iterate backwards)
-    @eight_row_controller = Controller::Grid.new(width: 8, bank: @bank).build.reverse
-    @four_row_controller = Controller::Grid.new(width: 4, bank: @bank).build.reverse
   end
 end
